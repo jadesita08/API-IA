0a3d5bee612d9d7f93cede12a69cf7e44ba68601
jadesita08 <jadeorona630@gmail.com>
2025-12-01 02:32:19 -0300

    Refactor Venta class to VentaModel
    Renamed Venta class to VentaModel and updated fields.

--- models/ventaModel.py
+++ models/ventaModel.py
@@ -1,9 +1,7 @@
-from pydantic import BaseModel  # Base para definir modelos con validación
+from pydantic import BaseModel
 
-class Venta(BaseModel):
-    # Esquema de la entidad Venta.
-    # Representa una transacción entre cliente y producto.
-    cliente_id: int  # ID del cliente que realiza la compra
-    producto_id: int # ID del producto vendido
-    cantidad: int    # Unidades vendidas del producto
-    total: float     # Total de la venta (precio * cantidad)
+class VentaModel(BaseModel):
+    id: int | None = None
+    cliente_id: int
+    producto_id: int
+    cantidad: int
