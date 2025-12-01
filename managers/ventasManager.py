d65d0cda86d7a5e2c753a90a6dc8c1dd4185a924
jadesita08 <jadeorona630@gmail.com>
2025-12-01 02:29:07 -0300

    Refactor ventasManager to use static methods

--- managers/ventasManager.py
+++ managers/ventasManager.py
@@ -1,38 +1,26 @@
-import psycopg2
 from managers.conexionManager import ConexionManager
-from models.ventaModel import Venta 
+from models.ventaModel import VentaModel
 
 class VentasManager:
-    def __init__(self):
-        self.conn_manager = ConexionManager()
-    
-    def crear_venta(self, venta: Venta):
-        try:
-            conn = self.conn_manager.get_connection()
-            if conn is None:
-                return None
-            with conn.cursor() as cursor:
-                cursor.execute(
-                    "INSERT INTO ventas (cliente_id, producto_id, cantidad, total) VALUES (%s, %s, %s, %s) RETURNING id",
-                    (venta.cliente_id, venta.producto_id, venta.cantidad, venta.total)
-                )
-                venta_id = cursor.fetchone()[0]
-                conn.commit()
-            conn.close()
-            return venta_id
-        except psycopg2.Error as e:
-            print("Error en crear_venta:", e)
-            return None
-    
-    def obtener_ventas(self):
-        try:
-            conn = self.conn_manager.get_connection()
-            if conn is None:
-                return []
-            with conn.cursor() as cursor:
-                cursor.execute("SELECT id, cliente_id, producto_id, cantidad, total, fecha_venta FROM ventas")
-                column_names = [desc[0] for desc in cursor.description]
-                ventas = []
-                for row in cursor.fetchall():
-                    venta_dict = dict(zip(column_names, row))
-                    if 'fecha_venta' in venta_dict and venta_dict['fecha
+    @staticmethod
+    def listar_ventas():
+        conn = ConexionManager.obtener_conexion()
+        cur = conn.cursor()
+        cur.execute("SELECT id, cliente_id, producto_id, cantidad FROM ventas")
+        rows = cur.fetchall()
+        cur.close()
+        conn.close()
+        return [{"id": r[0], "cliente_id": r[1], "producto_id": r[2], "cantidad": r[3]} for r in rows]
+
+    @staticmethod
+    def crear_venta(venta: VentaModel):
+        conn = ConexionManager.obtener_conexion()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO ventas (cliente_id, producto_id, cantidad) VALUES (%s, %s, %s) RETURNING id",
+                    (venta.cliente_id, venta.producto_id, venta.cantidad))
+        id = cur.fetchone()[0]
+        conn.commit()
+        cur.close()
+        conn.close()
+        return {"id": id, "cliente_id": venta.cliente_id, "producto_id": venta.producto_id, "cantidad": venta.cantidad}
+
