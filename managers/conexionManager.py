558041bc04cd4690b8a5414e411260463ad1734e
jadesita08 <jadeorona630@gmail.com>
2025-12-01 13:12:14 -0300

    Update conexionManager.py

--- managers/conexionManager.py
+++ managers/conexionManager.py
@@ -1,13 +1,19 @@
-import psycopg2
+import psycopg
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() 
 
 class ConexionManager:
-    @staticmethod
-    def obtener_conexion():
-        url = os.getenv("URLDATABASE")
-        if not url:
-            raise Exception("No se encontró la variable de entorno URLDATABASE")
-        return psycopg2.connect(url)
+    def __init__(self):
+        # Obtiene la URL de conexión
+        self.db_url = os.getenv('URLDATABASE')
+        
+    def get_connection(self):
+        """Establece la conexión a PostgreSQL. Lanza error si falla."""
+        try:
+            conn = psycopg.connect(self.db_url)
+            return conn
+        except psycopg.Error:
+            # En caso de error, retorna None para ser manejado en los Managers
+            return None
